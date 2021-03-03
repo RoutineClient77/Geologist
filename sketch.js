f3df6b75f44d03e0ff7c7ee542db8618375bcc7f
RoutineClient77 <77229720+RoutineClient77@users.noreply.github.com>
2021-03-03 16:04:36 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var hammer;
+var ground;
+var stone;
+var rubber;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	hammer = new Hammer(400,300,100,30);
+  ground = new Ground(600,height,1200,20);
+	stone = new Stone(600,200,100,100);
+	rubber = new Rubber(100,200,40,40);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("blue");
+  
+  drawSprites();
+  hammer.display();
+  ground.display();
+  stone.display();
+  rubber.display();
+}
+
+
+
